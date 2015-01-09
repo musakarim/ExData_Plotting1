9c2912694f411e03d00b3de36911004fd8bfd78f
musakarim <mkarim_nar@hotmail.com>
2015-01-09 15:49:45 +0500

    commit one

---
+++ plot1.R
@@ -0,0 +1,9 @@
+##loading data to R
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+##subseting loaded data as per required dates
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#extracting variable of interenst
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
